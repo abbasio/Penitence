ac1cb36441eccff8208a19a31cfc18c6cb8eb3fd
Omar <omarabbasi16@gmail.com>
2023-10-22 01:11:07 -0400

    removed scope comment

--- src/Scripts/Player.cs
+++ src/Scripts/Player.cs
@@ -87,16 +87,3 @@ public partial class Player : Area2D
 		QueueFree();
 	}
 }
-
-/* 
-Scope for project
-
-Wave based defense, divided into 'days'. Each day contains a morning and night portion.
-In the morning, zombies come to try and eat your brains. You will control your own body to fight them off, with a basic melee attack combo. (maybe ranged?)
-At night time, reapers come to try and harvest your soul. You control your spirit to defend yourself, defeating enemies by flying into them
-After a full day (morning + night cycle) you will get the opportunity to choose an upgrade. For example, more damage for your body or faster movement for the soul
-Picking one option will increase the difficulty of the opposite. Ie; faster soul but daytime zombies have more health
-Game goes on for 7 full days (7 mornings and 7 nights) before ending. Mb a small story attached
-
-
-*/
